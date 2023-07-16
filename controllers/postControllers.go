1bf02ab3bb9665ddc6fe7f7f503296f28fc7107c
henryanandsr <13521004@std.stei.itb.ac.id>
2023-07-17 00:35:43 +0700

    feat : add Post Company

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -4,6 +4,7 @@ import (
 	"SingleService-Labpro/initializers"
 	model "SingleService-Labpro/models"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -41,4 +42,49 @@ func PostBarang(c *gin.Context) {
 		KodeBarang:   request.KodeBarang,
 	}
 	initializers.DB.Create(barang)
+}
+
+func PostCompany(c *gin.Context) {
+	var request struct {
+		Nama   string `json:"nama"`
+		Alamat string `json:"alamat"`
+		NoTelp string `json:"no_telp"`
+		Kode   string `json:"kode"`
+	}
+
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": "Invalid data format",
+		})
+		return
+	}
+	if len(request.Kode) != 3 || strings.ToUpper(request.Kode) != request.Kode {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": "Invalid Kode, it must be all upper case and have a length of 3",
+		})
+		return
+	}
+	company := &model.Company{
+		Nama:      request.Nama,
+		Alamat:    request.Alamat,
+		NoTelepon: request.NoTelp,
+		KodePajak: request.Kode,
+	}
+
+	result := initializers.DB.Create(company)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "error",
+			"message": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"message": "Company created successfully",
+		"data":    company,
+	})
 }
--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"SingleService-Labpro/controllers"
+	"SingleService-Labpro/initializers"
+
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	initializers.LoadEnvVariables(".env")
+	initializers.ConnectToDB()
+}
 func main() {
-	
+	r := gin.Default()
+	r.POST("/barang", controllers.PostBarang)
+	r.POST("/company", controllers.PostCompany)
+	r.Run(":8080")
 }
