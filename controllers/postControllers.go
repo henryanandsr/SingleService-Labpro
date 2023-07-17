b1e4788e78def91891dff12daa532de9af3db45f
henryanandsr <13521004@std.stei.itb.ac.id>
2023-07-17 14:11:42 +0700

    fix : post barang & company

--- controllers/postControllers.go
+++ controllers/postControllers.go
@@ -4,11 +4,10 @@ import (
 	"SingleService-Labpro/initializers"
 	model "SingleService-Labpro/models"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
+	"github.com/google/uuid"
 )
 
 func PostBarang(c *gin.Context) {
@@ -19,14 +18,31 @@ func PostBarang(c *gin.Context) {
 		PerusahaanID string `json:"perusahaan_id"`
 		KodeBarang   string `json:"kodeBarang"`
 	}
-	// if request.HargaBarang <= 0 || request.StokBarang < 0 || request.PerusahaanID == "" {
-	// 	c.JSON(http.StatusBadRequest, gin.H{
-	// 		"status":  "error",
-	// 		"message": "Invalid data",
-	// 		"data":    nil,
-	// 	})
-	// 	return
-	// }
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": "Invalid JSON format",
+			"data":    nil,
+		})
+		return
+	}
+	if request.HargaBarang <= 0 || request.StokBarang < 0 || request.PerusahaanID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": "Invalid data",
+			"data":    nil,
+		})
+		return
+	}
+	var existingBarang model.Barang
+	if err := initializers.DB.Where("kode_barang = ?", request.KodeBarang).First(&existingBarang).Error; err == nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"status":  "error",
+			"message": "KodeBarang already exists",
+			"data":    nil,
+		})
+		return
+	}
 	var existingCompany model.Company
 	if err := initializers.DB.Where("id = ?", request.PerusahaanID).First(&existingCompany).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
@@ -36,29 +52,20 @@ func PostBarang(c *gin.Context) {
 		})
 		return
 	}
-	var existingBarang model.Barang
-	if !gorm.IsRecordNotFoundError(initializers.DB.Where("kode_barang = ?", request.KodeBarang).First(&existingBarang).Error) {
-		c.JSON(http.StatusConflict, gin.H{
-			"status":  "error",
-			"message": "KodeBarang already exists",
-			"data":    nil,
-		})
-		return
-	}
-	var highestIDBarang model.Barang
-	initializers.DB.Order("kode_barang desc").First(&highestIDBarang)
-	highestID, _ := strconv.Atoi(highestIDBarang.KodeBarang)
-	newID := highestID + 1
-	newIDString := strconv.Itoa(newID)
 
 	barang := &model.Barang{
-		KodeBarang:   newIDString,
+		KodeBarang:   request.KodeBarang,
 		NamaBarang:   request.NamaBarang,
 		HargaBarang:  request.HargaBarang,
 		StokBarang:   request.StokBarang,
 		IDPerusahaan: existingCompany.Nama,
 	}
 	initializers.DB.Create(barang)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"message": "Barang created successfully",
+		"data":    barang,
+	})
 }
 
 func PostCompany(c *gin.Context) {
@@ -83,13 +90,13 @@ func PostCompany(c *gin.Context) {
 		})
 		return
 	}
-	var highestIDCompany model.Company
-	initializers.DB.Order("id desc").First(&highestIDCompany)
-	highestID, _ := strconv.Atoi(highestIDCompany.ID)
-	newID := highestID + 1
-	newIDString := strconv.Itoa(newID)
+	// var highestIDCompany model.Company
+	// initializers.DB.Order("id desc").First(&highestIDCompany)
+	// highestID, _ := strconv.Atoi(highestIDCompany.ID)
+	// newID := highestID + 1
+	// newIDString := strconv.Itoa(newID)
 	company := &model.Company{
-		ID:        newIDString,
+		ID:        uuid.New().String(),
 		Nama:      request.Nama,
 		Alamat:    request.Alamat,
 		NoTelepon: request.NoTelp,
